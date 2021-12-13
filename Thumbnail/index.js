c19451f47897070da848c74530183de5057aba5a
arrajput01 <36032471+arrajput01@users.noreply.github.com>
2021-12-13 14:46:07 +0500

    added fucntion path added for product pics

--- Thumbnail/index.js
+++ Thumbnail/index.js
@@ -38,7 +38,7 @@ module.exports.image_resize =async (context, eventGridEvent, inputBlob) => {
   const widthInPixels = 150;
   const contentType = context.bindingData.data.contentType;
   const blobUrl = context.bindingData.data.url;
-  const blobName = blobUrl.slice(blobUrl.lastIndexOf("/")+1);
+  const blobName = blobUrl.slice(blobUrl.lastIndexOf("bringfresh/media/products/")+1);
 
   if(blobName.split('-md').length > 1)
   {
