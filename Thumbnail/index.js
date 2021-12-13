242f0c17143a4b6c4936b7f6018e3835ae9d5681
arrajput01 <36032471+arrajput01@users.noreply.github.com>
2021-12-13 14:22:48 +0500

    added fucntion enhansed

--- Thumbnail/index.js
+++ Thumbnail/index.js
@@ -35,7 +35,7 @@ module.exports.image_resize =async (context, eventGridEvent, inputBlob) => {
   context.log('image resizing started')
 
   const aborter = Aborter.timeout(30 * ONE_MINUTE);
-  const widthInPixels = 100;
+  const widthInPixels = 150;
   const contentType = context.bindingData.data.contentType;
   const blobUrl = context.bindingData.data.url;
   const blobName = blobUrl.slice(blobUrl.lastIndexOf("/")+1);
