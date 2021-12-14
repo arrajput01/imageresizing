678ffcdd316cce4c84c02ae3662c0e1905635fb8
arrajput01 <36032471+arrajput01@users.noreply.github.com>
2021-12-14 18:39:09 +0500

    image quality enhansed

--- Thumbnail/index.js
+++ Thumbnail/index.js
@@ -36,7 +36,7 @@ module.exports.image_resize =async (context, eventGridEvent, inputBlob) => {
 
   // const productURL = `${context}/media/products/`
   const aborter = Aborter.timeout(30 * ONE_MINUTE);
-  const widthInPixels = 80;
+  const widthInPixels = 150;
   const contentType = context.bindingData.data.contentType;
   const blobUrl = context.bindingData.data.url;
   console.log(blobUrl)
