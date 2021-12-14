a6dc8cdd3dbc1ceeedae14ec6d7352a31bb8d156
arrajput01 <36032471+arrajput01@users.noreply.github.com>
2021-12-14 22:21:03 +0500

    fix: changed width for imae

--- Thumbnail/index.js
+++ Thumbnail/index.js
@@ -36,7 +36,7 @@ module.exports.image_resize =async (context, eventGridEvent, inputBlob) => {
 
   // const productURL = `${context}/media/products/`
   const aborter = Aborter.timeout(30 * ONE_MINUTE);
-  const widthInPixels = 150;
+  const widthInPixels = 200;
   const contentType = context.bindingData.data.contentType;
   const blobUrl = context.bindingData.data.url;
   console.log(blobUrl)
@@ -59,7 +59,7 @@ module.exports.image_resize =async (context, eventGridEvent, inputBlob) => {
   context.log(`reading blob and resizing ${blobUrl}, ${blobName}, ${contentType}`)
 
   const image = await Jimp.read(inputBlob);
-  const thumbnail = image.resize(widthInPixels, Jimp.AUTO).quality(100);
+  const thumbnail = image.resize(widthInPixels, Jimp.AUTO);
   context.log(thumbnail)
   const thumbnailBuffer = await thumbnail.getBufferAsync(Jimp.AUTO);
   const readStream = stream.PassThrough();
